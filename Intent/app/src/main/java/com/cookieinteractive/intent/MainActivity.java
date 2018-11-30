99f5fcef3f52b2065bd49268f583c5f3f41c70a7
Daniel <cookieinter@gmail.com>
2018-11-30 20:29:21 +0900

    Intent - Base

--- Intent/app/src/main/java/com/cookieinteractive/intent/MainActivity.java
+++ Intent/app/src/main/java/com/cookieinteractive/intent/MainActivity.java
@@ -1,13 +1,30 @@
 package com.cookieinteractive.intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Intent intent;
+
+    Button buttonStartActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStartActivity = findViewById(R.id.button);
+
+        buttonStartActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        intent = new Intent(getApplicationContext(), SubActivity.class);
+        startActivity(intent);
     }
 }
