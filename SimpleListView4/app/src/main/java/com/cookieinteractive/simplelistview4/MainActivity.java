ddd637f66803fe8532d33a0ce44d17869ed3b340
Daniel <cookieinter@gmail.com>
2018-12-20 13:14:19 +0900

    완료.

--- SimpleListView4/app/src/main/java/com/cookieinteractive/simplelistview4/MainActivity.java
+++ SimpleListView4/app/src/main/java/com/cookieinteractive/simplelistview4/MainActivity.java
@@ -1,22 +1,17 @@
 package com.cookieinteractive.simplelistview4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;  // ListView 선언
 
-    MyCar mySportsCar;
-
     ArrayList<MyCar> myArrayList2;  // 커스텀 리스트 뷰를 위한 데이터 저장소
-    MyCar myCar_1;
+    MyCar myCar_1;  // 세부 데이터 - 서랍 한 칸에 해당.
     MyCar myCar_2;
     MyCar myCar_3;
 
@@ -25,20 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.list_view);
+        listView = findViewById(R.id.list_view);  // 객체 연결.
 
-        myArrayList2 = new ArrayList<>();
-        myCar_1 = new MyCar(10, "AAA");
+        myArrayList2 = new ArrayList<>();  // ArrayList 객체 생성.
+        myCar_1 = new MyCar(10, "AAA");  // myCar 객체 생성 + 내부 데이터 지정. (스피드, 메이커)
         myCar_2 = new MyCar(20, "BBB");
         myCar_3 = new MyCar(30, "CCC");
 
-        myArrayList2.add(myCar_1);
-        myArrayList2.add(myCar_2);
-        myArrayList2.add(myCar_3);
+        myArrayList2.add(myCar_1);  // 데이터 저장소에 세부 데이터(서랍 1칸씩) 추가.
+        myArrayList2.add(myCar_2);  // 두 번째 서랍도 추가.
+        myArrayList2.add(myCar_3);  // 세 번째도.
 
+        // 종류가 MyAdapter 이고, 변수명이 my_my_Adapter 인 커스텀 어댑터를,
+        // this(MainActivity)와 myArrayList2를 재료로 사용하여 만든다.
         MyAdapter my_my_Adapter = new MyAdapter(this, myArrayList2);
 
-        listView.setAdapter(my_my_Adapter);
-
+        listView.setAdapter(my_my_Adapter);  // 리스트뷰에 어댑터 연결(할당).
     }
 }
--- SimpleListView4/app/src/main/java/com/cookieinteractive/simplelistview4/MyAdapter.java
+++ SimpleListView4/app/src/main/java/com/cookieinteractive/simplelistview4/MyAdapter.java
@@ -1,5 +1,7 @@
 package com.cookieinteractive.simplelistview4;
 
+// 메뉴 표시줄의 Code >> Implement Methods (단축키 컨트롤 + i) 로 필수 메서드들을 자동생성할 수 있다.
+
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,35 +11,26 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MyAdapter extends BaseAdapter {  // 이 어댑터는 누가 사용? = 커스텀 리스트뷰
-    // 아까까지는 안드로이드에서 미리 만들어 둔 Simple List View 사용
-    // 저걸 사용하면서 Simple List Adapter 를 사용했다.
-    // 그 Simple List Adapter 도 안드로이드에서 미리 만들어 둔 것.
+public class MyAdapter extends BaseAdapter {
 
-    // 지금 만들고 있는 MyAdapter 는 커스텀 리스트뷰에서 사용할 어댑터이고,
-    // 내가 직업 만들어야 함.
+    Activity activity_a;  // 액티비티 선언 - 외부에서 재료로 전달받은 액티비티를 내부에서 사용하려는 목적.
 
-    // 이걸 누가 쓰냐? = 커스텀 리스트 뷰가 사용
-    // 그럼 이건 뭐냐? 리스트뷰 어댑터
+    ArrayList<MyCar> myArrayList_a;  // 마찬가지로 외부에서 재료로 전달받은 데이터를 내부에서 사용하려는 목적.
 
-    Activity activity;  // 액티비티 선언.
-
-    ArrayList<MyCar> myArrayList;  // 여기 안에 있는 내용을 리스트뷰에 표현.
-
-    MyAdapter(Activity activity_B, ArrayList arrayList_B){
-        activity = activity_B;
-        myArrayList = arrayList_B;
+    MyAdapter(Activity activity_b, ArrayList<MyCar> arrayList_b) {
+        activity_a = activity_b;  // 외부에서 받은 액티비티(activity_b)를 내부의 액티비티 변수(activity_a)로 연결.
+        myArrayList_a = arrayList_b;  // 외부에서 전달받은 데이터 저장소(arrayList_b)를 내부 데이터 저장소 변수(myArrayList_a)로 복사(연결).
     }
 
     @Override
-    public int getCount() {  // 겟(얻어와라, 가져와라) 카운드 = 데이터가 몇개나 들었는가?
-        return myArrayList.size();
+    public int getCount() {
+        return myArrayList_a.size();  // 리스트에 표시해야 할 데이터가 몇개인지 리턴.
     }
 
     @Override
-    public Object getItem(int position) {  // 아이템을 얻어와라.
+    public Object getItem(int position) {
         // ArrayList 안에 서랍 - 안의 내용
-        return myArrayList.get(position);
+        return myArrayList_a.get(position);  // position 에 해당하는 데이터를 러턴.
     }
 
     @Override
@@ -47,15 +40,15 @@ public class MyAdapter extends BaseAdapter {  // 이 어댑터는 누가 사용?
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater layoutInflater = activity.getLayoutInflater();
-        View view_item = layoutInflater.inflate(R.layout.listview_item, null);
+        LayoutInflater layoutInflater = activity_a.getLayoutInflater();  // 어댑터가 생성될 때 받은 액티비티로 레이아웃 인플레이터를 만든다.
+        View view_item = layoutInflater.inflate(R.layout.listview_item, null);  // 방금 생성한 레이아웃 인플레이터로 listview_item.xml 를 만들어서 view_item 에 할당.
 
-        TextView textView1 = view_item.findViewById(R.id.textView1);
-        TextView textView2 = view_item.findViewById(R.id.textView2);
+        TextView textView1 = view_item.findViewById(R.id.textView1);  // 예전에 MainActivity.java 에서 버튼을 연결해줬던 것 처럼 여기서도 listview_item.xml 의 텍스트뷰를
+        TextView textView2 = view_item.findViewById(R.id.textView2);  // 가져와서 연결해준다.
 
-        textView1.setText("" + myArrayList.get(position).speed);
-        textView2.setText(myArrayList.get(position).maker);
+        textView1.setText("" + myArrayList_a.get(position).speed);  // 텍스트뷰의 내용을 알맞게 설정한다. (각 셀에 맞게)
+        textView2.setText(myArrayList_a.get(position).maker);
 
-        return view_item;
+        return view_item;  // 다되었으면 리턴해준다.
     }
 }
